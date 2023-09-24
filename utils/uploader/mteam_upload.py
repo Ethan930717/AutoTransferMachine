b776b480c6d71f26e647b9153af9bf73e3df771b
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-24 23:38:25 +0800

    Create mteam_upload.py

---
+++ utils/uploader/mteam_upload.py
@@ -0,0 +1,224 @@
+from loguru import logger
+import time
+import os
+from atm.utils.uploader.upload_tools import *
+import re
+import cloudscraper
+
+def mteam_upload(siteinfo,file1,record_path,qbinfo,basic,hashlist):
+    post_url = "https://kp.m-team.cc/takeupload.php"
+    tags=[]
+    time_out=40
+    if (file1.pathinfo.type=='anime' or file1.pathinfo.type=='tv') and file1.pathinfo.collection==0:
+        fileinfo=file1.chinesename+'在'+siteinfo.sitename+'第'+file1.episodename+'集'
+    else:
+        fileinfo=file1.chinesename+'在'+siteinfo.sitename
+
+
+    #选择类型
+    if 'anime' in file1.pathinfo.type.lower():
+        select_type='405'
+    elif 'show' in file1.pathinfo.type.lower():
+        select_type='402'        
+    elif 'tv' in file1.pathinfo.type.lower() and file1.pathinfo.collection>=1:
+        if '480' in file1.standard_sel:
+            select_type='403'
+        else:
+            select_type='402'
+    elif 'movie' in file1.pathinfo.type.lower():
+        if 'remux' in file1.type.lower():
+            select_type='439'
+        elif 'dvd' in file1.type.lower() and 'iso' in file1.type.lower():
+            select_type='420'
+        elif 'blu' in file1.type.lower() or 'bdmv' in file1.type.lower() or 'iso' in file1.type.lower():
+            select_type='421'
+        elif '1080' in file1.standard_sel:
+            select_type='419'            
+        else:
+            select_type='419'
+    elif 'doc' in file1.pathinfo.type.lower():
+        select_type='404'
+    elif 'mv' in file1.pathinfo.type.lower():
+        select_type='406'
+    elif 'sport' in file1.pathinfo.type.lower():
+        select_type='407'
+    elif 'music' in file1.pathinfo.type.lower():
+        select_type='434'
+    else:
+        select_type='405'
+    logger.info('已成功填写类型为'+file1.pathinfo.type)
+
+    #选择编码
+    if 'H' in file1.pathinfo.video_format.upper() and '264' in file1.pathinfo.video_format:
+        codec_sel='1'
+        logger.info('已成功选择编码为H264/AVC')
+    elif 'x' in file1.pathinfo.video_format.lower() and '264' in file1.pathinfo.video_format:
+        codec_sel='1'
+        logger.info('已成功选择编码为H264/AVC')     
+    elif 'AVC' in file1.pathinfo.video_format:
+        codec_sel='1'
+        logger.info('已成功选择编码为H264/AVC')                
+    elif 'H' in file1.pathinfo.video_format.upper() and '265' in file1.pathinfo.video_format:
+        codec_sel='16'
+        logger.info('已成功选择编码为H265/HEVC')
+    elif 'x' in file1.pathinfo.video_format.lower() and '265' in file1.pathinfo.video_format:
+        codec_sel='16'
+        logger.info('已成功选择编码为H265/HEVC')    
+    elif 'HEVC' in file1.pathinfo.video_format.upper():
+        codec_sel='16'
+        logger.info('已成功选择编码为H265/HEVC')                
+    elif 'MPEG-4' in file1.pathinfo.video_format.upper():
+        codec_sel='15'
+        logger.info('已成功选择编码为MPEG')
+    elif 'MPEG-2' in file1.pathinfo.video_format.upper():
+        codec_sel='4'
+        logger.info('已成功选择编码为MPEG')                     
+    elif 'VC' in file1.pathinfo.video_format.upper():
+        codec_sel='2'
+        logger.info('已成功选择编码为VC1')          
+    elif 'XVID' in file1.pathinfo.video_format.upper():
+        codec_sel='3'
+        logger.info('已成功选择编码为XVID')
+    elif 'FLAC' in file1.pathinfo.video_format.upper():
+        codec_sel='5'
+        logger.info('已成功选择编码为FLAC') 
+    elif 'DTS' in file1.pathinfo.video_format.upper():
+        codec_sel='11'
+        logger.info('已成功选择编码为DTS')
+    elif 'AC' in file1.pathinfo.video_format.upper() and '3' in file1.pathinfo.video_format.upper():
+        codec_sel='12'
+        logger.info('已成功选择编码为AC3')
+    elif 'AAC' in file1.pathinfo.video_format.upper():
+        codec_sel='18'
+        logger.info('已成功选择编码为DTS')                                              
+    else:
+        codec_sel='1'
+        logger.info('未识别到视频编码信息，已选择others')  
+
+    #选择分辨率
+    if '8K' in file1.standard_sel:
+        standard_sel='6'
+    elif '2160' in file1.standard_sel:
+        standard_sel='6'
+    elif '1080p' in file1.standard_sel.lower():
+        standard_sel='1'
+    elif '1080i' in file1.standard_sel.lower():
+        standard_sel='2'
+    elif '720' in file1.standard_sel:
+        standard_sel='3'
+    elif '480' in file1.standard_sel:
+        standard_sel='5'
+    else:
+        standard_sel='1'
+    logger.info('已成功选择分辨率为'+file1.standard_sel)
+    
+    #选择地区
+    if not file1.country=='':
+        if '大陆' in file1.country:
+            processing_sel='1'
+            logger.info('国家信息已选择'+file1.country)
+        elif '香港' in file1.country:
+            processing_sel='3'
+            logger.info('国家信息已选择'+file1.country)
+        elif '台湾' in file1.country:
+            processing_sel='3'
+            logger.info('国家信息已选择'+file1.country)
+        elif '美国' in file1.country:
+            processing_sel='2'
+            logger.info('国家信息已选择'+file1.country)
+        elif '英国' in file1.country:
+            processing_sel='2'
+            logger.info('国家信息已选择'+file1.country)
+        elif '法国' in file1.country:
+            processing_sel='6'
+            logger.info('国家信息已选择'+file1.country)
+        elif '韩国' in file1.country:
+            processing_sel='5'
+            logger.info('国家信息已选择'+file1.country)
+        elif '日本' in file1.country:
+            processing_sel='4'
+            logger.info('国家信息已选择'+file1.country)
+        elif '印度' in file1.country:
+            processing_sel='6'
+            logger.info('国家信息已选择'+file1.country)
+        else:
+            processing_sel='6'
+            logger.info('未找到资源国家信息，已选择其他')
+    else:
+        processing_sel='4'
+        logger.info('未找到资源国家信息，已默认日本')
+
+    #选择制作组
+    if 'MTeam' in file1.sub.upper():
+        team_sel='9'
+    elif 'LowPower-Raws' in file1.sub.upper():
+        team_sel='22'
+    else:
+        team_sel='0'
+    logger.info('制作组已成功选择为'+file1.sub)
+
+    
+    if siteinfo.uplver==1:
+        uplver='yes'
+    else:
+        uplver='no'
+
+    torrent_file = file1.torrentpath
+    file_tup = ("file", (os.path.basename(torrent_file), open(torrent_file, 'rb'), 'application/x-bittorrent')),
+            
+    other_data = {
+            "type": select_type,
+            "name": file1.uploadname,
+            "small_descr": file1.small_descr+file1.pathinfo.exinfo,
+            "url": file1.imdburl,
+            "color": "0",
+            "font": "0",
+            "size": "0",
+            "descr": file1.content,
+            "codec_sel": codec_sel,
+            "standard_sel": standard_sel,
+            "processing_sel" : processing_sel,
+            "team_sel": team_sel,
+            "uplver": uplver,
+            }
+    headers = {
+        'authority': 'kp.m-team.cc',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        'cache-control': 'max-age=0',
+        'cookie': siteinfo.cookie,
+        'origin': 'https://kp.m-team.cc',
+        'referer': 'https://kp.m-team.cc/upload.php',
+        'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62',
+    }
+    if '国' in file1.language or '中' in file1.language:
+        other_data['l_dub']='yes'
+        logger.info('已选择国语')
+    if not file1.sublan=='' and ('简' in file1.sublan or '繁' in file1.sublan or '中' in file1.sublan):
+        other_data['l_sub']='yes'
+        logger.info('已选择中字')
+    scraper=cloudscraper.create_scraper()
+    
+    success_upload=0
+    try_upload=0
+    while success_upload==0:
+        try_upload+=1
+        if try_upload>5:
+            return False,fileinfo+' 发布种子发生请求错误,请确认站点是否正常运行'
+        logger.info('正在发布种子')
+        try:
+            r = scraper.post(post_url, headers=headers,cookies=cookies_raw2jar(siteinfo.cookie),data=other_data, files=file_tup,timeout=time_out)
+            success_upload=1
+        except Exception as r:
+            logger.warning('发布种子发生错误: %s' %(r))
+            success_upload=0
+        
+    return afterupload(r,fileinfo,record_path,siteinfo,file1,qbinfo,hashlist)
