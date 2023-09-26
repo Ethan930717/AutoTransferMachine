6c584ade3c296cf646bff2036ef99aea78a7536e
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-27 04:12:09 +0800

    Update atm_install.sh

--- packages/atm_install.sh
+++ packages/atm_install.sh
@@ -14,7 +14,10 @@ else
 echo "输入地址有误，请重新输入！"
 fi
 done
-echo "开始安装环境"
+echo "检查安装环境"
+sudo rm -rf /usr/local/lib/python3.9/dist-packages/AutoTransferMachine
+sudo rm /usr/local/bin/atm
+echo "开始安装"
 sudo apt update && sudo apt upgrade
 sudo apt install -y wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
@@ -24,17 +27,15 @@ cd Python-3.10.0
 ./configure --enable-optimizations
 make altinstall
 echo "删除安装包"
-sudo rm -rf /home/Python-3.10.0
-sudo rm /home/Python-3.10.0.tgz
+sudo rm -rf $dir/Python-3.10.0
+sudo rm $dir/Python-3.10.0.tgz
 sudo python3 -m pip install --upgrade pip
-echo "安装ATM依赖"
-sudo apt-get install -y python3-pip ffmpeg mediainfo mktorrent screen unzip git
 echo "从Git克隆项目"
 cd $dir
 git clone https://github.com/Ethan930717/AutoTransferMachine.git AutoTransferMachine
 echo "转移openpyxl包"
 cd $dir/AutoTransferMachine/packages
-unzip openpyxl.zip -d /usr/local/lib/python3.9/dist-packages/
+unzip -o openpyxl.zip -d /usr/local/lib/python3.9/dist-packages/
 echo "删除openpyxl压缩包"
 sudo rm openpyxl.zip
 echo "生成转种配置文件"
@@ -43,11 +44,12 @@ echo "删除配置文件压缩包"
 sudo rm atm.zip
 echo "转移命令行配置"
 mv atm /usr/local/bin/
+chmod +x /usr/local/bin/atm
 echo "转移ATM包"
 cd $dir
-mv * /usr/local/lib/python3.9/dist-packages/
+mv AutoTransferMachine /usr/local/lib/python3.9/dist-packages/
 echo "删除package文件夹"
-sudo rm package
+sudo rm packages
 echo "测试运行情况"
 atm -h
 
