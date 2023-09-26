ca3a2a5f1f1cfb53308278a414ac7a49ff10c30e
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-26 20:03:28 +0800

    Update readyaml.py

--- utils/para_ctrl/readyaml.py
+++ utils/para_ctrl/readyaml.py
@@ -1,14 +1,14 @@
 import os
 import yaml
 from loguru import logger
-from AutoTransferMachine.utils.getinfo.info_transfer import download_torrents
+from AutoTransferMachine.utils.getinfo.info_transfer import get_download_url
 
 def readyaml(file):
     newfile=file+'.bak'
     logger.info('正在读取yaml...')
     f=open(file, encoding='utf-8')
     audata = yaml.load(f, Loader=yaml.FullLoader)
-    download_torrents(audata)
+    get_download_url(audata)
     try:
         if (len(audata)<1):
             logger.warning('配置文件'+file+'数据少于一行，判断为数据缺失，已自动使用备份配置文件'+newfile)
