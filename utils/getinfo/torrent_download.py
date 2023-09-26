2dd2ecd8c42fb1a7298c23618ca9dceb4799ab9c
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-26 22:50:45 +0800

    Update torrent_download.py

--- utils/getinfo/torrent_download.py
+++ utils/getinfo/torrent_download.py
@@ -15,7 +15,7 @@ def cookies_raw2jar(raw_cookies):
         cookie_dict[key] = value
     return cookiejar_from_dict(cookie_dict)
 
-def get_torrent(audata):
+def get_torrent(yamlinfo):
     choosesite = input(f"请选择你要下载种子的网站\n1.影")
     if choosesite == "1":
         sitename = "shadowflow"
@@ -49,7 +49,7 @@ def get_torrent(audata):
                     ws["F1"] = "下载链接"
                     for tr in trs:
                         if "禁转" in tr.text or "分集" in tr.text : # 这里可以排除不想爬的种子标签
-                            print(f"{details}\n不符合筛选条件，跳过")
+                            print(f"不符合筛选条件，跳过")
                             continue
                         try:
                             embedded = tr.find("td", class_="embedded")
@@ -91,5 +91,20 @@ def get_torrent(audata):
     total_pages = page + 1
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"爬取结束，本次共读取到{total_rows}个种子,耗时{execution_time},脚本结束")
-    sys.exit(0)
+    logger.info(f"爬取结束，本次共读取到{total_rows}个种子,耗时{execution_time}，请选择接下来的任务\n 1.批量打印种子链接 2.批量打印下载链接 3.结束ATM")
+    choice = input("请输入您的选择：")
+    if choice == "1":
+        print("以下是所有的种子链接：")
+    for i in range(2, row):
+        details = ws["E" + str(i)].value
+    print(details)
+    elif choice == "2":
+    print("以下是所有的下载链接：")
+    for i in range(2, row):
+        download = ws["F" + str(i)].value
+    print(download)
+    elif choice == "3":
+    print("感谢您使用ATM，再见！")
+    sys.exit(0)  # 结束脚本
+    else:
+    print("您输入的选项不正确，请重新输入！")
