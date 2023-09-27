21d5915843add18eacd58695b41ba7a95c2f7aec
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-27 22:45:51 +0800

    Update info_transfer.py

--- utils/getinfo/info_transfer.py
+++ utils/getinfo/info_transfer.py
@@ -5,7 +5,7 @@ from lxml import etree
 import requests
 from requests.cookies import cookiejar_from_dict
 from AutoTransferMachine.utils.getinfo.makeyaml import mkyaml
-import csv
+import openpyxl
 
 def cookies_raw2jar(raw_cookies): # 定义一个函数，将原始的cookie字符串转换为cookiejar对象
     cookie_dict = {}
@@ -23,9 +23,10 @@ def find_key_by_value(dict, siteurl):
     return None
 
 def getmediainfo(yamlinfo):
-    with open(yamlinfo['basic']['torrent_list'], 'r', encoding='gbk') as csv_file:
-        reader = csv.reader(csv_file)
-        url_list = [cell for row in reader for cell in row if "detail" in cell]
+    wb = openpyxl.load_workbook(yamlinfo['basic']['torrent_list'])
+    ws = wb.active
+    data = ws.values
+    url_list = [cell for row in data for cell in row if "detail" in cell]
     writemode = input(f"请选择模板转换方式\nY.在原有的pathinfo下自动续写\nN.覆盖原有的pathinfo，从path1开始生成（默认自动续写）")
     print(reader)
     print(yamlinfo['basic']['torrent_list'])
