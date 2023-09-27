fda8bb60d5bd22432deba9fcce75c58c17b669e5
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-27 14:59:40 +0800

    Update para_ctrl.py

--- utils/para_ctrl/para_ctrl.py
+++ utils/para_ctrl/para_ctrl.py
@@ -4,6 +4,8 @@ from AutoTransferMachine.utils.para_ctrl.readyaml import write_yaml
 
 import os
 from loguru import logger
+import csv
+import urllib
 
 def read_para():
     args = readargs()
@@ -11,10 +13,9 @@ def read_para():
     iu=0#img upload
     su=0#sign
     ru=0#resources upload
-    if not args.img_upload+args.sign+args.upload+args.douban_info+args.media_img+args.download==1:
+    if not args.img_upload+args.sign+args.upload+args.douban_info+args.media_img+args.download+args.transinfo==1:
         logger.error('参数输入错误，上传模式 -u,签到模式 -s,拉种模式 -dl, 上传图床模式 -iu,获取豆瓣信息 -di, 获取视频截图链接 -mi, 必须且只能选择一个。')
         raise ValueError ('参数输入错误，上传模式 -u,签到模式 -s,上传图床模式 -iu,获取豆瓣信息 -di, 获取视频截图链接 -mi, 必须且只能选择一个。')
-
 
     au_data   = readyaml(args.yaml_path)
     basic_data = readyaml(args.basic_path)
@@ -36,6 +37,7 @@ def read_para():
     write_yaml(au_data)
     
     au_data['mod']=args.media_img*'media_img'+args.img_upload*'img_upload'+args.sign*'sign'+args.upload*'upload'+args.douban_info*'douban_info'+args.download*'download'
+
 
     if args.upload:
         if not 'path info' in au_data or len(au_data['path info'])==0:
@@ -119,14 +121,7 @@ def read_para():
         if 'img_num' in args and not (args.img_num=='' or args.img_num==None) :
             au_data['basic']['picture_num']=int(args.img_num)
 
-
         au_data['media_file']=args.media_file
-
-
-
-
-
-
 
     return au_data
 
