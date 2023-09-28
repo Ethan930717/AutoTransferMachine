659042d26a799f7e0f88bddd26b9d7ab956e8346
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-29 00:06:24 +0800

    Create __init__.py

---
+++ AutoTransferMachine/utils/para_ctrl/__init__.py
@@ -0,0 +1,6 @@
+#import upload_machine.utils.para_ctrl.readyaml
+#import upload_machine.utils.para_ctrl.readargs
+
+#readyaml = upload_machine.utils.para_ctrl.readyaml.readyaml
+#readargs = upload_machine.utils.para_ctrl.readargs.readargs
+
