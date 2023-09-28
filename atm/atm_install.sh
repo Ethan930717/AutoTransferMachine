fe296f80a6908b9b8167e4102dd7c66d91b7779d
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-09-29 02:30:54 +0800

    Update atm_install.sh
    完善模板信息

--- atm/atm_install.sh
+++ atm/atm_install.sh
@@ -22,31 +22,29 @@ mkdir torrent_path record_path screenshot_path
 echo "创建功能文件夹成功"
 cat > au.yaml << EOF
 basic:
-  picture_num: 6   #默认自动截图的张数，不设置就是3
-  workpath: /home/atm/     #au.yaml所在文件夹的地址
-  doubancookie: #豆瓣的cookie
-  new_folder: 0   #不用动
-  check: 0   #不用动basic:
-  picture_num: 6   #默认自动截图的张数，不设置就是3
-  workpath: /home/atm/     #au.yaml所在文件夹的地址
-  doubancookie: #豆瓣的cookie
-  new_folder: 0   #不用动
-  check: 0   #不用动
-  record_path: /home/atm/record_path   
-  screenshot_path: /home/atm/screenshot_path   #这三个是上面那个地址里面的三个子文件夹，跟au.yaml同级
-  torrent_path: /home/atm/torrent_path
-  torrent_list: /home/atm/record_path/shadowflow_torrents.xlsx  
-  #这个是存放种子目录的xlsx表格，使用拉种模式后会自动生成表格，可以选择让脚本自动写入这个地址
-  serverchan_api: SCT149255TZvd5PvM******* #server酱api，发种结束后可以push推送通知
-  tmdb_api: #tmdbapi
+  picture_num: 6                 #默认自动截图的张数，不设置就是3
+  workpath: /home/atm/           #au.yaml所在文件夹的地址
+  doubancookie:                  #豆瓣的cookie
+  new_folder: 0                  #不用动
+  check: 0                       #不用动
+  picture_num: 6                 #默认自动截图的张数，不设置就是3
+  workpath: /home/atm/           #au.yaml所在文件夹的地址
+  new_folder: 0                  #不用动
+  check: 0                       #不用动
+  record_path: /home/atm/record_path           #记录类文件存放路径
+  screenshot_path: /home/atm/screenshot_path   #截图暂存路径
+  torrent_path: /home/atm/torrent_path         #种子下载目录
+  torrent_list: /home/atm/record_path/shadowflow_torrents.xlsx   #种子信息文档路径，工具的dl指令会自动生成
+  serverchan_api: SCT149************            #server酱的sendkey，发种结束后可以push推送通知
+  tmdb_api:                                     #tmdb的api，没有就空着
 
 qbinfo:
-  qburl: 192.168.5.1:8080  #QB地址
-  qbwebuiusername: admin   #用户名
+  qburl: 192.168.5.1:8080   #QB地址
+  qbwebuiusername: admin    #用户名
   qbwebuipassword: admin    #密码
-  start: 1  #选1则进种自动开始，选0进种暂停，默认跳校验
-image hosting:
-  seq:#这边可以调整图床优先级顺序
+  start: 1                  #选1则进种自动开始，选0进种暂停，默认跳校验
+image hosting:              #图床
+  seq:                      #这边可以调整图床优先级顺序
     1: smms
     2: pter
     3: freeimage
@@ -54,7 +52,7 @@ image hosting:
   pter:
     cookie: KEEP_LOGIN_GOAUTH=vrksE%3Aecd5c7992a32ff3***********  #cookie直接F12从控制台复制抓取即可
     url: https://s3.pterclub.com
-  dahuimg: #大胡图床还没做好，占个坑
+  dahuimg:
     cookie: 
     url: https://img.dahu.fun/
   smms:
@@ -62,39 +60,345 @@ image hosting:
   freeimage:
     cookie: 
     url: https://freeimage.host/
-#下面几个已经取消适配了，但是自动调整yaml时还是会添加，后续会删除
-  picgo:
-    url: https://www.picgo.net/api/1/upload
-  emp:
-    url: https://jerking.empornium.ph
-  chd:
-    url: https://shewang.net/
 
-site info:  #这里添加站点信息
-  shadowflow:  #转出不转进的站点也需要添加进去，要抓cookie跟passkey
-    passkey: 4e1fb7c4e*******
-    url: https://shadowflow.org/
-    enable: 0  #0就是不启用，转种的时候会自动跳过， 1就是启用
-    uplver: 0  #0是不匿名发种，1是匿名
-    cookie: *******
-  ilolicon:
-    url: https://share.ilolicon.com/
-    enable: 1
+site info:  #站点信息,为方便大家使用，所有适配的站点全部加到了，需要的站点填上信息，然后将enable设为1即可启用
+  audience:
+    enable: 0
+    cookie: ''
+    passkey: ''
     uplver: 0
-    passkey: 
-    cookie: 
-  hdpost:  #hdpost适配不上，写出来，希望有大佬帮忙
+    url: https://audiences.me/
+  btschool: #学校未适配完成
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.btschool.club/
+  carpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://carpt.net/
+  cyanbug:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://cyanbug.net/
+  dajiao:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://dajiao.cyou
+  discfan:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://discfan.net/
+  dragonhd:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.dragonhd.xyz/
+  freefarm:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.0ff.cc/
+  gtk:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.gtk.pw/
+  haidan:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.haidan.video/
+  hares:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://club.hares.top/
+  hdarea:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdarea.club/
+  hddolby:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.hddolby.com/
+  hdfans:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdfans.org/
+  hdhome:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdhome.org/
+  hdmayi:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdmayi.com/
+  hdpost: #普斯特未适配完成
+    enable: 0
+    cookie: ''
+    uplver: 0
     url: https://pt.hdpost.top/
-    enable: 1
+  hdpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
     uplver: 0
-    cookie:
+    url: https://hdpt.xyz/
+  hdtime:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdtime.org/
+  hdu:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.hdupt.com/
+  hdvideo:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdvideo.one/
+  hdzone:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hdfun.me/
+  hhclub:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hhanclub.top/
+  hitpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.hitpt.com/
+  hudbt: #蝴蝶为适配完成
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://hudbt.hust.edu.cn/
+  icc:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.icc2022.com/
+  ilolicon:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://share.ilolicon.com/
+  itzmx:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.itzmx.com/
+  joyhd:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.joyhd.net/
+  kufei:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://kufei.org/
+  mangguo:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.3wmg.com/
+  mteam:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://kp.m-team.cc/
+  nanyang:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://nanyangpt.com/
+  okpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.okpt.net/
+  oshen:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.oshen.win/
+  pandapt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pandapt.net/
   piggo:
-    url: https://piggo.me/
-    enable: 1
+    enable: 0
+    cookie: ''
+    passkey: ''
     uplver: 0
-    cookie: 
+    url: https://piggo.me/
+  ptcafe:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://ptcafe.club/
+  ptchina:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://ptchina.org/
+  pter:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pterclub.com/
+  pthome:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.pthome.net/
+  ptlsp:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.ptlsp.com/
+  ptsbao: #烧包未适配完成
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://ptsbao.club/
+  pttime:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://www.pttime.org/t
+  redleaves:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://leaves.red/
+  rousi:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://rousi.zip/
+  shadowflow:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://shadowflow.org/
+  soulvoice:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.soulvoice.club/
+  tccf:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://et8.org/
+  ubits:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://ubits.club/
+  wintersakura:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://wintersakura.net/
+  wuerpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://52pt.site/
+  ydy:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://pt.hdbd.us/
+  yiptba:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://1ptba.com/
+  zhuque:   #朱雀需要另外添加torrentkey，在朱雀个人信息中可以查看
+    enable: 0
+    cookie: ''
+    torrentkey: ''
+    uplver: 0
+    url: https://zhuque.in/
+  zmpt:
+    enable: 0
+    cookie: ''
+    passkey: ''
+    uplver: 0
+    url: https://zmpt.cc/
 
-#以下是转种的资源信息，了解为主，测试阶段可以手动添加，后续脚本适配会自动生成
+#以下是转种的资源信息，了解为主，脚本会自动生成
 path info:
   path1:
     downloadpath: /download #这里是QB内部的下载地址
@@ -111,7 +415,7 @@ path info:
     sub: YingWEB    #制作组，从主标题抓取，自动生成
     type: movie   #类型，同上
     contenthead: #这里写简介头的内容，也就是感谢发布的位置
-        contenttail:  #这里写简介尾的内容，适用性较少
+    contenttail:  #这里写简介尾的内容，适用性较少
     audio_format: DDP5.1   #音频
     video_format: H264     #视频
     medium: WEB-DL   #媒介
@@ -120,7 +424,6 @@ path info:
     screenshot: null #如果需要自选截图，则这边直接复制图片链接即可,长度随意，注意要把basic_num数量改为0，否则默认还是会自动截图
     from_url: https://mikanani.me/
     imdb_url: null
-    carpt: 0
 EOF
 echo "创建yaml配置模板成功，创建启动指令文件"
 cd /usr/local/bin/
