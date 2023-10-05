a16966e1346d835e677d4aa37502fbcd6dd3e782
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2023-10-05 18:55:13 +0800

    Update atm_install.sh

--- atm_install.sh
+++ atm_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-echo "请输入您本地的视频文件存放目录,如/home/media"
+#安装python
+echo "请输入您本地的视频文件存放目录"
 valid=false
 while [ $valid = false ]
 do
@@ -17,17 +18,10 @@ parent=$(dirname "$dir")
 atm="$parent/atm"
 mkdir -p "$atm"
 echo "创建atm文件夹成功"
+sudo mv $dir $atm
 cd atm
 curl -o "$atm/atm.zip" https://pan.dahu.fun/d/file/atm.zip?sign=yjLNnG5S4oD21ZIIC-0y02CFZSRq5TJAccdhRtfKVeQ=:0
 unzip "$atm/atm.zip" -d "$atm"
 cd $atm
 echo "atm配置文件加载成功，您的视频文件夹路径前级已变更为atm"
 docker-compose run atm
-
-
-
-
-
-
-
-
